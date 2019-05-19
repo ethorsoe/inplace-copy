7ded7c98653a6a4f18c8617e29e93ca9e96df4aa
Emil Karlson <jekarlson@gmail.com>
2019-05-19 19:47:55 +0300

    import libc and add BufferedFile struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 #![ macro_use ]
 
 pub extern crate nix;
+pub extern crate libc;
 
 use std::env;
 use std::fs::File;
 use std::os::unix::io::AsRawFd;
+
+const BUFFER_SIZE: usize = 4096;
+
+struct BufferedFile {
+	file: File,
+	buffer: [u8; BUFFER_SIZE],
+}
 
 fn main() {
 	if env::args_os().count() != 3 {
@@ -12,7 +20,10 @@ fn main() {
 	}
 	let mut args_iter = env::args_os().into_iter();
 	args_iter.next();
-	let fhandles : Vec<File> = args_iter.map(|s| File::open(&s).expect((String::from("No such file ") + s.into_string().expect("File with unprintable name can't be opened").as_str()).as_str())).collect();
-	let first_fd = fhandles[0].as_raw_fd();
+	let fhandles : Vec<BufferedFile> = args_iter.map(|s| BufferedFile {
+		file: File::open(&s).expect((String::from("No such file ") + s.into_string().expect("File with unprintable name can't be opened").as_str()).as_str()),
+		buffer: [0; BUFFER_SIZE],
+	}).collect();
+	let first_fd = fhandles[0].file.as_raw_fd();
 	println!("firstfd {}", &first_fd);
 }
