2501ac59a3a56d3ecc06199a3ae4e149f2079f17
Emil Karlson <jekarlson@gmail.com>
2019-05-19 18:20:42 +0300

    inital commit of code stolen from scrub repo

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,18 @@
-extern crate blkid;
-extern crate btrfs;
+#![ macro_use ]
 
-use blkid::dev::Devs;
-use blkid::cache::Cache;
-use btrfs::linux;
+pub extern crate nix;
+
 use std::env;
 use std::fs::File;
 use std::os::unix::io::AsRawFd;
 
-fn do_blkid(uuid: &str) {
-	let cache = Cache::new().unwrap();
-	for device in Devs::new(&cache) {
-		let mut is_btrfs = false;
-		let mut has_right_uuid = false;
-		
-		for (tag, value) in device.tags() {
-			if tag == "TYPE" &&  value == "btrfs" {
-				is_btrfs = true;
-			} else if tag == "UUID" && value == uuid {
-				has_right_uuid = true;
-			}
-		}
-		if is_btrfs  && has_right_uuid {
-			let path = device.name();
-			println!("identified: {}", path.to_string_lossy());
-		} 
-	}
-}
-
 fn main() {
-	if env::args_os().count() != 2 {
-		panic!("usage: {} <path>", env::args_os().nth(0).unwrap().into_string().unwrap());
+	if env::args_os().count() != 3 {
+		panic!("usage: {} <infile> <outfile>", env::args_os().nth(0).unwrap().into_string().unwrap());
 	}
-	let path = env::args_os().nth(1).unwrap().into_string().unwrap();
-	let fhandle = File::open(&path).unwrap();
-	let root_fd = fhandle.as_raw_fd();
-	let fs_info = linux::get_filesystem_info(root_fd).unwrap();
-	let uuid = fs_info.filesystem_id.hyphenated().to_string();
-	println!("Filesystem {} with {} devices", &uuid, fs_info.num_devices);
-
-	do_blkid(&uuid);
+	let mut args_iter = env::args_os().into_iter();
+	args_iter.next();
+	let fhandles : Vec<File> = args_iter.map(|s| File::open(&s).expect((String::from("No such file ") + s.into_string().expect("File with unprintable name can't be opened").as_str()).as_str())).collect();
+	let first_fd = fhandles[0].as_raw_fd();
+	println!("firstfd {}", &first_fd);
 }
