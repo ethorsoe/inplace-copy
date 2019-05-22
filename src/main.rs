424aa5434b1bf20b54ada04aff3f129ee86073f8
Emil Karlson <jekarlson@gmail.com>
2019-05-22 22:23:31 +0300

    only open outfile as writable

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,17 @@ fn copy_block(fhandles: &mut Vec<BufferedFile>, offset: u64) -> bool {
 	return last_read == BUFFER_SIZE as u64;
 }
 
+fn get_buffered_file(filename: std::ffi::OsString, write: bool) -> BufferedFile {
+	let file = std::fs::OpenOptions::new().read(true).write(write).open(&filename)
+		.expect((String::from("No such file ") + filename.into_string()
+		.expect("File with unprintable name can't be opened").as_str()).as_str());
+	BufferedFile {
+		file: file,
+		last_read: 0,
+		buffer: [0; BUFFER_SIZE],
+	}
+}
+
 fn main() {
 	if env::args_os().count() != 3 {
 		panic!("usage: {} <infile> <outfile>", env::args_os().nth(0).unwrap().into_string()
@@ -52,13 +63,9 @@ fn main() {
 	}
 	let mut args_iter = env::args_os().into_iter();
 	args_iter.next();
-	let mut fhandles : Vec<BufferedFile> = args_iter.map(|s| BufferedFile {
-		file: std::fs::OpenOptions::new().read(true).write(true).open(&s)
-			.expect((String::from("No such file ") + s.into_string()
-			.expect("File with unprintable name can't be opened").as_str()).as_str()),
-		last_read: 0,
-		buffer: [0; BUFFER_SIZE],
-	}).collect();
+	let infile = get_buffered_file(args_iter.next().unwrap(), false);
+	let outfile = get_buffered_file(args_iter.next().unwrap(), true);
+	let mut fhandles : Vec<BufferedFile> = vec!(infile, outfile);
 	let mut counter: u64 = 0;
 	while copy_block(&mut fhandles, counter) {
 		counter += BUFFER_SIZE as u64;
